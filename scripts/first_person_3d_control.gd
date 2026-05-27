b2ace116355d2abf4930d5921222a21ac29d28a6
عقیل نوروزی | Alan <76964438+LAMEK8206@users.noreply.github.com>
2026-05-27 09:37:15 +0330

    چنتا ویژگی جدید اضافه شده
    به دلیل قطعی اینترنت یادم رفته دقیقا چی کار کردم ولی  تا جای که یادمه نشستن و دویدن و یه چیز دیگه اضافه کردم

--- scripts/first_person_3d_control.gd
+++ scripts/first_person_3d_control.gd
@@ -2,24 +2,58 @@ extends CharacterBody3D
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity") #گرفتن مقدار جاذبه از انجین
 
-@export var player_speed : float = 6.0 #سرعت حرکت
-@export var jump_force : float = 8.0 #قدرت پرش
+#----------------------------------------------------------------------------------------------
+# مقییر ها برای تایین کلید های وردی برای کنترل پلیر
+@export var forward_key: String 
+@export var backward_key : String
+@export var left_key: String
+@export var right_key: String
+@export var jump_key: String 
+@export var sit_key: String
+@export var run_key : String
+@export var flashlight_key: String
+#----------------------------------------------------------------------------------------------
+#پارمتر های قابل تغییر برای پلیر
+@export var player_walk_speed : float = 6.0 #سرعت حرکت
+@export var player_run_speed : float = 8.5 #سرعت دویدن
+@export var player_sit_speed : float = 2.5 #سرعت نشتن
+var player_move_speed : float # متغییر برای تظیم تغییر بین حالت دویدون و راه رفتن
+@export var player_jump_force : float = 6.0 #قدرت پرش
 @export var mouse_sensitivity : float = 0.5 # سرعت چرخش دوربین
 
-#متقیر ها برای نشستن و بلند شدن
-#var stand_height := 1.8
-#var crouch_height := 1.0
+#----------------------------------------------------------------------------------------------
+#نود های استفاده شده در کد
+@onready var stand_collision : CollisionShape3D = $stand_collision
+@onready var sit_collision : CollisionShape3D = $sit_collision
+@onready var plaer_camer : Node3D = $plaer_camer
+@onready var flash_light : SpotLight3D = $plaer_camer/flash_light
+@onready var raycast : RayCast3D = $raycast
+@onready var animation_camera: AnimationPlayer = $plaer_camer/Camera3D/animation_camera
 
-@onready var plaer_camer: Node3D = $plaer_camer
-#@onready var collision: CollisionShape3D = $collision
 
+#----------------------------------------------------------------------------------------------
+# متغییر ها مربط به حالت ایستادن و نشتن پلیر
+var stand_position_camera  # پوزیشن دوربین در حالت ایستاده
+var sit_position_camera  # پوزیشن دوربین در حالت نشسته
+enum sit_statos {stand , sit , co_sit} # حالت های پلیر
+var player_statos : sit_statos = sit_statos.stand # تغییر حالت های پلیر (کنترل کردن حالت ها )
 
-@export var input_key : Array [Key] = [KEY_W , KEY_S , KEY_A , KEY_D ,KEY_SPACE , KEY_SHIFT]
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED) #قفل کردن موس در لحظاه ای اجرای بازی
-
-
+	
+	# تنظیم حالت اولیه کولیژن ها و تنظیم مقادیر که دوربین باید به انها حرکت کند
+	stand_collision.disabled = false
+	sit_collision.disabled = true
+	stand_position_camera = plaer_camer.position.y
+	sit_position_camera = plaer_camer.position.y /2
+	
+	# مقدار اولیه سرعت حرکتر پلیر
+	player_move_speed = player_walk_speed
+	
+	# تنظیم حالت اولیه ای چراغ قوه
+	flash_light.visible = false
+	
 func _process(delta: float) -> void:
 	#کپچر کردن و ازاد کردن موس
 	if Input.is_action_just_pressed("ui_cancel"): 
@@ -28,7 +62,12 @@ func _process(delta: float) -> void:
 		else :
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 			
-
+	#--------------------------------------------------------------------------------	
+	
+	# فراخوانی تابع ها (برای تمیز تر شدن کد)
+	player_sit(delta) # تابع نشتن
+	flashlight() # تابع چراغ‌قوه
+	camera_animation() # تابع کمنترل انمیشن های دوربین
 
 func _unhandled_input(event: InputEvent) -> void:
 	#چرخاندن دوربین با استفاده از موس  و محدود کردن چرخش در محور ایکس
@@ -39,40 +78,94 @@ func _unhandled_input(event: InputEvent) -> void:
 	
 func _physics_process(delta: float) -> void:
 	
-	#مکانیک نشستن
-	#var shape := collision.shape as CapsuleShape3D
-	
-	#if Input.is_action_pressed("crouch"):
-		#shape.height = crouch_height
-		#plaer_camer.position.y = crouch_height
-	#else :
-		#shape.height = stand_height
-		#plaer_camer.position.y = stand_height
-	
-	
 	#اعمال جاذبه
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-	#پرش
-	if Input.is_key_pressed(input_key[4]) and is_on_floor() : 
-		velocity.y += jump_force
-	#var debug = Input.is_key_pressed(input_key[0])
+	
+#--------------------------------------------------------------------------------
+
+	# فراخوانی تابع ها (برای تمیز تر شدن کد)
+	player_move_ment() # تابع حرکت به چهار چهت پلیر
+	player_jump()# تابع پرش پلیر 
+	
+	
+	
+	move_and_slide() # تابع مورد نیاز برای اعمال فیزیکی (اجباری و پیشفرض)
+
+
+
+func  player_move_ment () : 
+	
 	#حرکت به چپ و راست روبه رو و عقب
 	#گرفتن جهت
 	var input_dir = Vector2(
-		int(Input.is_key_pressed(input_key[3])) - int(Input.is_key_pressed(input_key[2])) ,
-		int(Input.is_key_pressed(input_key[1])) - int(Input.is_key_pressed(input_key[0]))
+		int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left")) ,
+		int(Input.is_action_pressed("backward")) - int(Input.is_action_pressed("forward"))
 		)
 	var diction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized() #تبدیل به بردار سه بعدی
 	#اعمال نیروی حرکتی 
 	if diction:
-		if Input.is_key_pressed(input_key[5]) and Input.is_key_pressed(input_key[1]) == false :  
-			velocity.x = diction.x * player_speed * 2
-		else :
-			velocity.x = diction.x * player_speed
-		velocity.z = diction.z * player_speed
+		velocity.x = diction.x * player_move_speed
+		velocity.z = diction.z * player_move_speed
 	else:
 		velocity.x = 0
 		velocity.z = 0
 	
-	move_and_slide()
+	# دویدن پلیر
+	if Input.is_action_pressed("run") and Input.is_action_pressed("forward") : 
+		player_move_speed = player_run_speed
+	else :
+		player_move_speed = player_walk_speed
+
+func player_jump () : 
+	#پرش
+	if Input.is_action_just_pressed(jump_key) and is_on_floor() : 
+		velocity.y += player_jump_force
+
+func player_sit (delta) : 
+		
+	#اعمال نسشت با استفاده از اینام وضیعت کاراکتر
+	match player_statos : 
+		sit_statos.stand:
+			stand_collision.disabled = false
+			sit_collision.disabled = true
+			player_move_speed = player_walk_speed
+			plaer_camer.position.y = lerp(plaer_camer.position.y , stand_position_camera , 6 *delta)
+		sit_statos.sit : 
+			stand_collision.disabled = true
+			sit_collision.disabled= false
+			player_move_speed = player_sit_speed
+			plaer_camer.position.y = lerp(plaer_camer.position.y , sit_position_camera , 6 *delta)
+		sit_statos.co_sit : 
+			stand_collision.disabled = true
+			sit_collision.disabled= false
+			player_move_speed = player_sit_speed
+			plaer_camer.position.y = lerp(plaer_camer.position.y , sit_position_camera , 6 *delta)
+	# کنترل اینام وضعیت کاراکتر
+	if Input.is_action_pressed(sit_key) : 
+		player_statos = sit_statos.sit
+	elif raycast.is_colliding() :
+		player_statos = sit_statos.sit
+	else :
+		player_statos = sit_statos.stand
+
+func flashlight () :
+	if Input.is_action_just_pressed(flashlight_key) and flash_light.visible == false : 
+		flash_light.visible = true
+	elif Input.is_action_just_pressed(flashlight_key) and flash_light.visible == true :
+		flash_light.visible = false
+
+func camera_animation () : 
+	if Input.is_action_pressed(forward_key) : 
+		animation_camera.play("walke_animation")
+	else :
+		animation_camera.play("idel_camera")
+	if Input.is_action_pressed(forward_key) and Input.is_action_pressed(run_key) : 
+		animation_camera.play("run_camera")
+	else :
+		animation_camera.play("idel_camera")
+	if Input.is_action_pressed(sit_key) :
+		if raycast.is_colliding() and Input.is_action_pressed(forward_key) : 
+			animation_camera.play("sit_animation")
+	else :
+		animation_camera.play("idel_camera")
